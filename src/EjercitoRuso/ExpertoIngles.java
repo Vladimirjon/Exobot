f6f5da6d3900e52105d245be9af8bb991a0c9765
Shimi <jorgea.torresm@hotmail.com>
2024-02-07 23:42:02 -0500

    Interfaz de los expertos

--- src/EjercitoRuso/ExpertoIngles.java
+++ src/EjercitoRuso/ExpertoIngles.java
@@ -1,5 +1,7 @@
 package EjercitoRuso;
 
-public class ExpertoIngles {
-
+public class ExpertoIngles implements ITecnicoIngles{
+    public void traducirInglesTecnico(){
+        System.out.println("Estoy aprendiendo a traducir y entender el idioma ingles");
+    }
 }

78a5c91186441d52ae0b54b46f2f95feda12857c
JosDavP <jose.pallares@epn.edu.ec>
2024-02-13 18:11:48 -0500

    Pierna y reactor

--- src/Exoesqueleto/Pierna.java
+++ src/Exoesqueleto/Pierna.java
@@ -1,5 +1,27 @@
 package Exoesqueleto;
 
 public class Pierna {
+    private String piernaIzquierda;
+    private String piernaDerecha;
+    private String habilidad;
 
+    public Pierna(String piernaIzquierda, String piernaDerecha, String hanilidad){
+
+    
+        this.piernaIzquierda = piernaIzquierda;
+        this.piernaDerecha = piernaDerecha;
+    }
+    //Metodos getter y setter para las variables de la clase
+    public String getPiernaIzquierda() {
+        return piernaIzquierda;
+    }
+    public String  getPiernaDerecha() {
+        return piernaDerecha;
+    }
+    public String  getHabilidad() {
+        return habilidad;
+    }
+    public void habilidad(){
+        System.out.println("Las piernas ahora pueden correr a grandes velocidades y dar saltos muy altos");
+    }
 }
--- src/Exoesqueleto/TurboReactor.java
+++ src/Exoesqueleto/TurboReactor.java
@@ -1,5 +1,24 @@
 package Exoesqueleto;
 
 public class TurboReactor {
+    private int carga;
+    private boolean vuelo;
 
+    public TurboReactor(){
+        this.carga= 0;
+        this.vuelo= false;
+    }
+    public void cargarCombustible(int cantidad){
+        carga += cantidad;
+        System.out.println("Se ha cargado "+ cantidad+"%.");
+    }
+    public void activarVuelo() {
+        if (carga>0) {
+            vuelo = true;
+            System.out.println("El exoesqueleto puede volar");
+            } else{
+                System.out.println("El exoesqueleto  no tiene combustible suficiente para el vuelo");
+}
+
+}
 }
